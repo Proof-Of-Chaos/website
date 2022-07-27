66be23b023382c4e6dfec52697e74331f013ebf2
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-07-27 02:34:33 +0200

    no swr on static page

--- src/pages/nfts.js
+++ src/pages/nfts.js
@@ -1,23 +1,19 @@
 import Layout from '../layouts/layout'
 import NFTDetail from '../components/nft/nft-detail'
-import { nftsFetcher, useNfts } from '../lib/hooks/use-nfts'
-import { SWRConfig } from 'swr'
+import { nftsFetcher } from '../lib/hooks/use-nfts'
 
-function PageNFTs({fallback}) {
+function PageNFTs({ nfts }) {
 
-  const { nfts, loading, error } = useNfts();
+  // const { nfts, loading, error } = useNfts();
 
   return (
-    <SWRConfig value={{ fallback }}>
-      <section className="mx-auto w-full max-w-[1160px] text-sm sm:pt-10 4xl:pt-14">
-        { loading && <div>loading...</div> }
-        { error && <div>error getting nfts</div> }
-        { !loading && !error && nfts && nfts.map( nft => <NFTDetail key={ nft.id } nft={ nft } /> ) }
+    <section className="mx-auto w-full max-w-[1160px] text-sm sm:pt-10 4xl:pt-14">
 
-        <pre className="text-red-500 text-center">... implement a fetch to get more NFTs ...</pre>
-        <pre className="text-red-500 text-center">src/lib/hooks/use-nfts.js</pre>
-      </section>
-    </SWRConfig>
+      { nfts && nfts.map( nft => <NFTDetail key={ nft.id } nft={ nft } /> ) }
+
+      <pre className="text-red-500 text-center">... implement a fetch to get more NFTs ...</pre>
+      <pre className="text-red-500 text-center">src/lib/hooks/use-nfts.js</pre>
+    </section>
   )
 }
 
@@ -32,9 +28,7 @@ export async function getStaticProps() {
   const nfts = await nftsFetcher()
   return {
     props: {
-      fallback: {
-        'nfts': nfts,
-      }
+      nfts,
     },
     revalidate: 60 * 60, // In seconds
   }
