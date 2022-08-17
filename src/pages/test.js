d38a2eb756dc5b447e522a9f7cf313ba70930957
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-08-17 12:57:18 +0200

    fix test page error

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,12 +3,9 @@ import Layout from '../layouts/layout'
 import { useUserNfts } from '../lib/hooks/use-nfts'
 
 function Test() {
-
-  const { data: userNFTs } = useUserNfts( "DT7kRjGFvRKxGSx5CPUCA1pazj6gzJ6Db11xmkX4yYSNK7m" )
-  const userNFTSymbols = userNFTs.map( ( { symbol } ) => symbol )
   return (
     <>
-      { JSON.stringify(userNFTSymbols) }
+      Test Page
     </>
   )
 }
