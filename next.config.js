e0bde3278953fb313578b0e20752c804bed7c320
Gabriel <gabrieljaeger1@gmail.com>
2022-09-06 10:46:22 +0200

    renaming exportTrailingSlash

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self' ipfs.rmrk.link; script-src 'none'; sandbox;",
   },
-  exportTrailingSlash: true,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
