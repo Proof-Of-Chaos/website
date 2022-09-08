bdfbd8746c5c70773bee22c38ea2c148d73658bf
Thomas Moeskops <thomasmoeskops@brightfish.nl>
2022-09-08 16:01:39 +0200

    Fixed vote percentage + wip for treshold / proposal support

--- src/lib/hooks/use-referendums.js
+++ src/lib/hooks/use-referendums.js
@@ -6,7 +6,20 @@ import { useQuery } from "@tanstack/react-query";
 import useAppStore from "../../zustand";
 import { microToKSM, microToKSMFormatted } from "../utils";
 
-const BLOCK_DURATION = 6000;  
+const BLOCK_DURATION = 6000;
+const THRESHOLD_SUPERMAJORITYAPPROVE = 'SuperMajorityApprove'
+const THRESHOLD_SUPERMAJORITYAGAINST = 'SuperMajorityAgainst'
+const THRESHOLD_SIMPLEMAJORITY = 'SimpleMajority'
+
+const convictionMultiplierMapping = {
+  'None': 0.1,
+  'Locked1x': 1,
+  'Locked2x': 2,
+  'Locked3x': 3,
+  'Locked4x': 4,
+  'Locked5x': 5,
+  'Locked6x': 6,
+}
 
 export const referendumFetcher = async (ksmAddress) => {
   const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
@@ -14,18 +27,46 @@ export const referendumFetcher = async (ksmAddress) => {
 
   const { hash, number } = await api.rpc.chain.getHeader();
   const timestamp = await api.query.timestamp.now.at(hash);
-
+  const totalIssuance = await api.query.balances.totalIssuance().toString()
   const activeReferendums = await api.derive.democracy.referendums()
 
   let referendums = [];
   for (const referendum of activeReferendums) {
+    /* DOODLE LOOKING AT TRESHOLD / finding proposals (probably requires Subsquid API)
+    console.log(referendum.imageHash.toString());
+    const info = await api.query.democracy.referendumInfoOf(referendum.index)
+    console.log(info.toHuman())
+    const proposalhash = info.unwrap().asOngoing.proposalHash
+    console.log(proposalhash.toString())
+    let props = await api.query.democracy.preimages.entries()
+    //console.log(props)
+    props.forEach(([_, maybePreImage]) => {
+      let perImage = maybePreImage.unwrapOrDefault();
+      console.log(perImage.asAvailable.data.toString())
+    })*/
+    //const info = await api.query.democracy.referendumInfoOf(referendum.index)
+    //const proposalhash = info.unwrap().asOngoing.proposalHash
+    //console.log("!",referendum.imageHash.toString())
+
     const endDate = await getEndDateByBlock(referendum.status.end, number, timestamp)
     const PAData = await getPADataForRef(referendum.index.toString());
-    referendums.push(referendumObject(referendum, endDate, PAData, ksmAddress));
+    const threshold = getPassingThreshold(referendum, totalIssuance)
+    referendums.push(referendumObject(referendum, threshold, endDate, PAData, ksmAddress));
   }
 
   return referendums.sort((a,b)=>parseInt(a.id)-parseInt(b.id));
 };
+
+const getPassingThreshold = (referendum, totalIssuance) => {
+  return true; /* TODO: incorporate treshold formulas */
+  switch (referendum.status.threshold.toString()) {
+    case THRESHOLD_SUPERMAJORITYAPPROVE:
+      break;
+    case THRESHOLD_SIMPLEMAJORITY:
+    default:
+      return parseInt(referendum.votedTotal.toString()) / 2
+  }
+}
 
 const getEndDateByBlock = (blockNumber, currentBlockNumber, currentTimestamp) => {
   let newStamp = parseInt(currentTimestamp.toString()) + ((parseInt(blockNumber.toString()) - currentBlockNumber.toNumber()) * BLOCK_DURATION)
@@ -79,12 +120,28 @@ const parseCastVote = (vote) => {
   }
 }
 
-const referendumObject = (referendum, endDate, PAData, ksmAddress) => {
+const referendumObject = (referendum, threshold, endDate, PAData, ksmAddress) => {
   let title = PAData?.title
 
   if (!title && referendum.image) {
     title = referendum.image.proposal.section.toString() + '.' + referendum.image.proposal.method.toString()
   }
+
+  /* DOODLE LOOKING AT CONVICTION
+  let ayeTotal = 0;
+  let nayTotal = 0;
+  referendum.votes.forEach((vote) => {
+    const balance = parseInt(vote.vote?.conviction?.toString())
+    const convictionMultiplier = convictionMultiplierMapping[vote.vote?.conviction?.toString()]
+    const votePower = balance * convictionMultiplier
+    if (vote.vote?.isAye) {
+      ayeTotal += votePower
+    } else {
+      nayTotal += votePower
+    }
+  });
+  console.log(ayeTotal, nayTotal)*/
+  const votedTotal = parseInt(referendum.votedAye.toString()) + parseInt(referendum.votedNay.toString())
 
   return {
     id: referendum.index.toString(),
@@ -92,12 +149,12 @@ const referendumObject = (referendum, endDate, PAData, ksmAddress) => {
     voteVolume: microToKSM(referendum.votedTotal.toString()),
     aye: {
       vote: referendum.voteCountAye,
-      percentage: toPercentage(referendum.votedAye.toString(), referendum.votedTotal.toString()),
+      percentage: toPercentage(referendum.votedAye.toString(), votedTotal),
       voteVolume: microToKSMFormatted(referendum.votedAye.toString()),
     },
     nay: {
       vote: referendum.voteCountNay,
-      percentage: toPercentage(referendum.votedNay.toString(), referendum.votedTotal.toString()),
+      percentage: toPercentage(referendum.votedNay.toString(), votedTotal),
       voteVolume: microToKSMFormatted(referendum.votedNay.toString()),
     },
     executed_at: endDate,
@@ -110,6 +167,7 @@ const referendumObject = (referendum, endDate, PAData, ksmAddress) => {
     questions: {},
     description: PAData?.content ?? "-",
     isPassing: referendum.isPassing,
+    threshold: threshold,
   }
 }
 
