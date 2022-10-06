1948ca283386fce09174e890e0d075f8c7bc6a95
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-10-06 20:30:32 +0200

    really fix logo loading

--- src/components/ui/logo.js
+++ src/components/ui/logo.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from '../../../public/proof-of-chaos-logo.png'
 
 export default function Logo(props) {
   return (
@@ -13,7 +12,7 @@ export default function Logo(props) {
         <div className="logo-wrap h-12 w-12 relative">
           <Image
             className="h-12 w-12 relative"
-            src={logo}
+            src="/proof-of-chaos-logo.png"
             alt="Proof of Chaos Logo"
             layout='fill'
             objectFit='contain'
