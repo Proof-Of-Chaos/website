9bdfb571a0674b0975c04ba0331f7183414f57bd
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-10-15 16:42:44 +0200

    add minValue calc

--- src/components/modals/past-ref-modal.js
+++ src/components/modals/past-ref-modal.js
@@ -106,12 +106,16 @@ export default function PastReferendumModal( { id } ) {
     return <Loader />
   }
 
+  const isVoteBelowMinValue = values.ksm < refConfig?.minValue
   const currentLuckMultiplier = getLuckMultiplier( values.dragon, refConfig )
   const lucks = lucksForConfig( values.ksm, refConfig, currentLuckMultiplier )
-  const chartData = [
+
+  const chartData = ! isVoteBelowMinValue ? [
     { x: `common\n${ parseFloat(lucks['common']).toFixed(2) }%`, y: lucks['common'] },
     { x: `rare\n${ parseFloat(lucks['rare']).toFixed(2) }%`, y: lucks['rare'] },
     { x: `epic\n${ parseFloat(lucks['epic']).toFixed(2) }%`, y: lucks['epic'] },
+  ] : [
+    { x: `common\n${ parseFloat(lucks['common']).toFixed(2) }%`, y: lucks['common'] }
   ]
 
   const handleCheckBoxChange = ( e ) => {
@@ -173,8 +177,8 @@ export default function PastReferendumModal( { id } ) {
       <Dialog.Title as="h3" className="text-2xl font-medium leading-6 text-gray-900 pb-2">
         Past Referendum Details for #{ id }
       </Dialog.Title>
-      <div className="flex justify-center pt-4">
-        <div className="form-wrap w-1/2">
+      <div className="flex flex-wrap justify-center pt-4">
+        <div className="form-wrap w-full md:w-1/2">
           <h3 className="text-xl">Wallet Properties when voting / sendout</h3>
           <ThemeProvider theme={ theme }>
             <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
@@ -189,6 +193,7 @@ export default function PastReferendumModal( { id } ) {
                   }
                   label="Quiz Answered Correctly"
                 /> */}
+                <FormHelperText>See how the luck distribution differs for different values</FormHelperText>
                 <FormControlLabel
                   control={
                     <Checkbox
@@ -232,19 +237,19 @@ export default function PastReferendumModal( { id } ) {
                 <FormHelperText>Your Luck Multiplier is: { currentLuckMultiplier }</FormHelperText>
                 <div className="pt-5">
                   Vote Amount With Conviction
+                  { isVoteBelowMinValue && <FormHelperText>Your chance of getting a common NFT is 100% as your vote amount considered is below the minValue set for this referendum</FormHelperText> }
                   <Slider
                     min={ 0 }
                     max={ refConfig?.maxValue ? refConfig.maxValue + 10 : 50 }
                     marks={ marks }
                     value={ sliderValue }
-                    step={ 0.1 }
+                    step={0.1}
                     aria-label="Default"
                     valueLabelDisplay="auto"
                     onChange={ handleSliderChange }
                   />
                 </div>
               </FormGroup>
-              <FormHelperText>See how the luck distribution differs for different values</FormHelperText>
               <Button
                 onClick={ resetToUserVote }
                 variant={ isWalletSettingsShowing ? 'disabled' : 'calm' }
@@ -257,8 +262,8 @@ export default function PastReferendumModal( { id } ) {
             </FormControl>
           </ThemeProvider>
         </div>
-        <div className="chart-wrap w-1/2">
-          <h3 className="text-xl">Your Chances for NFTs</h3>
+        <div className="chart-wrap w-full md:w-1/2">
+          <h3 className="text-xl md:text-right">Your Chances for NFTs</h3>
           <VictoryPie
             padAngle={2}
             data={ chartData }
