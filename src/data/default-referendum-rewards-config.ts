9542652e6cd896f1d8d4f23ce44f16bba955b878
Gabriel <gabrieljaeger1@gmail.com>
2023-08-27 20:40:24 +0800

    37 is public dev collection

--- src/data/default-referendum-rewards-config.ts
+++ src/data/default-referendum-rewards-config.ts
@@ -18,7 +18,7 @@ export const defaultReferendumRewardsConfig: RewardConfiguration = {
       "This is the config NFT for the referendum rewards. You can use this NFT to verify the configuration that was used for the specific sendout.",
   },
   collectionConfig: {
-    id: parseInt(process.env.NEXT_PUBLIC_DEFAULT_COLLECTION_ID) || 34,
+    id: parseInt(process.env.NEXT_PUBLIC_DEFAULT_COLLECTION_ID) || 37,
     name: "",
     description: "",
     isNew: false,
